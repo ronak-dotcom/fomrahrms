b7e2f4d118aaef28a2db08f5578c68b7d1292ed7
ronak-dotcom <ronak@fomrahousing.in>
2026-08-31 08:45:09 +0530

    fix(employee): import leave_store for LeaveApprovalStatus
    my_cycle_blocks.dart referenced LeaveApprovalStatus and LeaveApplication
    without importing models/leave_store.dart, failing the web compile.

--- lib/widgets/my_cycle_blocks.dart
+++ lib/widgets/my_cycle_blocks.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../models/app_user.dart';
+import '../models/leave_store.dart';
 import '../models/user_session.dart';
 import '../services/supabase_service.dart';
 import '../services/user_store.dart';
